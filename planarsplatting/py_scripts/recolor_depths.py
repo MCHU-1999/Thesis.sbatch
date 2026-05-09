426d8364cf0a10fe06a2fa4c2fb00fecbdd8c3c5
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-05-09 03:22:49 +0200

    go

--- planarsplatting/py_scripts/recolor_depths.py
+++ planarsplatting/py_scripts/recolor_depths.py
@@ -124,7 +124,7 @@ def main():
             depth_data = np.load(npy_file)
 
             # plot_threshold_depth_map(depth_data, 4.0, npy_file.name)
-            # plot_depth_map(depth_data, npy_file.name)
+            plot_depth_map(depth_data, npy_file.name)
             
             # Normalize depth map
             # depth_data = np.clip(depth_data, 0, 100)
